5038aac6788bb2080f13120922321323253655f4
nikolavuko <nvuk0001@student.monash.edu>
2025-09-01 20:27:43 +1000

    Made it so charts are appearing on homepage

---
+++ src/components/TennisDashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "../App";  // <-- this is your chart code
+
+export default function TennisDashboard() {
+  return (
+    <div style={{ margin: "40px auto", maxWidth: 1200, padding: "0 16px" }}>
+      <App />
+    </div>
+  );
+}
+
